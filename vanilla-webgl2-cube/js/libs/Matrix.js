c315b49391b7340bc4b2751fc1d7bf5717142e8e
takumifukasawa <fukapon912@gmail.com>
2021-06-20 18:43:42 +0900

    remove debug

--- vanilla-webgl2-cube/js/libs/Matrix.js
+++ vanilla-webgl2-cube/js/libs/Matrix.js
@@ -13,63 +13,35 @@ export class Matrix4 {
       m30, m31, m32, m33
     ]
   }
+
   // fov ... rad
   // aspect ... w / h
   static getPerspectiveMatrix(fov, aspect, near, far) {
-    // var f = 1.0 / Math.tan(fov / 2);
-    // var rangeInv = 1 / (near - far);
-    // // prettier-ignore
-    // return new Matrix4(
-    //   f / aspect, 0, 0, 0,
-    //   0, f, 0, 0,
-    //   0, 0, (near + far) * rangeInv, -1,
-    //   0, 0, near * far * rangeInv * 2, 0
-    // );
-
-    // const f = 1 / Math.tan(fov / 2);
-    const f = Math.tan(Math.PI * 0.5 - 0.5 * fov);
+    const f = 1 / Math.tan(fov / 2);
     const rangeInv = 1 / (near - far);
-    // const s = Math.tan(Math.PI * 0.5 - 0.5 * fov);
-    // const frustumRange = far - near;
-    // const invFrustumRange = 1 / frustumRange;
-    // const invFrustumPosition = -near;
-
-    // const t = near * Math.tan(fov / 2);
-    // const r = t * aspect;
-    // const a = r * 2;
-    // const b = t * 2;
-    // const c = far - near;
 
     const m00 = f / aspect;
-    // const m00 = (near * 2) / a;
     const m01 = 0;
     const m02 = 0;
     const m03 = 0;
 
     const m10 = 0;
     const m11 = f;
-    // const m11 = (near * 2) / b;
     const m12 = 0;
     const m13 = 0;
 
     const m20 = 0;
     const m21 = 0;
     const m22 = (near + far) * rangeInv;
-    // const m22 = -(far + near) / c;
     const m23 = -1;
 
     const m30 = 0;
     const m31 = 0;
     const m32 = near * far * rangeInv * 2;
-    // const m23 = -(far * near * 2) / c;
     const m33 = 0;
 
     // prettier-ignore
     return new Matrix4(
-      // f / aspect, 0, 0, 0,
-      // 0, f, 0, 0,
-      // 0, 0, (near + far) * rangeInv, -1,
-      // 0, 0, near * far * rangeInv * 2, 0
       m00, m01, m02, m03,
       m10, m11, m12, m13,
       m20, m21, m22, m23,
@@ -85,6 +57,8 @@ export class Matrix4 {
       v.x, v.y, v.z, 1
     );
   }
+
+  // TODO: to vector4 class
   static multiplyVector4(mat, v) {
     const m = mat.elements;
     // prettier-ignore
@@ -100,40 +74,41 @@ export class Matrix4 {
       v.x * m03 + v.y * m13 + v.z * m23 + v.w * m33,
     ];
   }
+
   static multiplyMatrices(ma, mb) {
     const a = ma.elements;
     const b = mb.elements;
     const a00 = a[0],
-      a10 = a[1],
-      a20 = a[2],
-      a30 = a[3],
-      a01 = a[4],
+      a01 = a[1],
+      a02 = a[2],
+      a03 = a[3],
+      a10 = a[4],
       a11 = a[5],
-      a21 = a[6],
-      a31 = a[7],
-      a02 = a[8],
-      a12 = a[9],
+      a12 = a[6],
+      a13 = a[7],
+      a20 = a[8],
+      a21 = a[9],
       a22 = a[10],
-      a32 = a[11],
-      a03 = a[12],
-      a13 = a[13],
-      a23 = a[14],
+      a23 = a[11],
+      a30 = a[12],
+      a31 = a[13],
+      a32 = a[14],
       a33 = a[15];
     const b00 = b[0],
-      b10 = b[1],
-      b20 = b[2],
-      b30 = b[3],
-      b01 = b[4],
+      b01 = b[1],
+      b02 = b[2],
+      b03 = b[3],
+      b10 = b[4],
       b11 = b[5],
-      b21 = b[6],
-      b31 = b[7],
-      b02 = b[8],
-      b12 = b[9],
+      b12 = b[6],
+      b13 = b[7],
+      b20 = b[8],
+      b21 = b[9],
       b22 = b[10],
-      b32 = b[11],
-      b03 = b[12],
-      b13 = b[13],
-      b23 = b[14],
+      b23 = b[11],
+      b30 = b[12],
+      b31 = b[13],
+      b32 = b[14],
       b33 = b[15];
 
     // TODO: fix calc
--- vanilla-webgl2-cube/js/script.js
+++ vanilla-webgl2-cube/js/script.js
@@ -14,33 +14,6 @@ const states = {
   isResized: false,
 };
 
-(() => {
-  // prettier-ignore
-  // const m = new Matrix4(
-  //   1, 0, 0, 0,
-  //   0, 1, 0, 0,
-  //   0, 0, 1, 0,
-  //   2, 0, 0, 1
-  // );
-  // const m = Matrix4.getTranslateMatrix({ x: 1, y: 2, z: 3 });
-  const m = Matrix4.getPerspectiveMatrix(
-    // ( * Math.PI) / 360,
-    // 0.5,
-    // Math.PI * 0.5,
-    0.5,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    10
-  );
-  const v = {
-    x: 1,
-    y: 1,
-    z: -5,
-    w: 1,
-  };
-  const p = Matrix4.multiplyVector4(m, v);
-  console.log(p);
-})();
 class Mesh {
   constructor({ geometry, material }) {
     this.geometry = geometry;
@@ -64,21 +37,7 @@ class Mesh {
       10
     );
 
-    // const v = {
-    //   x: 0.5,
-    //   y: 0.5,
-    //   z: 0.5,
-    //   w: 1,
-    // };
-    // const p = Matrix4.multiplyVector4(projectionMatrix, v);
-    // console.log(p);
-
     gl.uniformMatrix4fv(loc, false, projectionMatrix.elements);
-    // gl.uniformMatrix4fv(
-    //   loc,
-    //   false,
-    //   Matrix4.getTranslateMatrix({ x: 0.5, y: 0, z: 0 }).elements
-    // );
     gpu.draw();
   }
 }
@@ -89,33 +48,22 @@ layout (location = 0) in vec3 aPosition;
 layout (location = 1) in vec3 aColor;
 
 out vec3 vColor;
-out vec4 vPosition;
 
 uniform mat4 uProjectionMatrix;
 
 void main() {
   vColor = aColor;
   vec4 pos = uProjectionMatrix * vec4(aPosition, 1.);
-  pos.z = 0.5;
-  pos.w = 1.;
-  // pos.z = 0.9;
-  // pos.w = 4.;
-  vPosition = pos;
   gl_Position = pos;
-  // gl_Position = vec4(aPosition, 1.);
-  // gl_Position = vec4(pos.xy, -1, 1);
 }
 `;
 
 const fragmentShader = `#version 300 es
 precision mediump float;
 in vec3 vColor;
-in vec4 vPosition;
 out vec4 outColor;
 void main() {
   outColor = vec4(vColor, 1);
-  // float zw = vPosition.z / vPosition.w;
-  // outColor = vec4(zw, 0., 0., 1.);
 }
 `;
 
