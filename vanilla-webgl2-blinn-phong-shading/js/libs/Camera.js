08c96557a8c414b8140fcb5872335229e54fff82
takumifukasawa <fukapon912@gmail.com>
2021-06-29 23:15:29 +0900

    blinn phong

---
+++ vanilla-webgl2-blinn-phong-shading/js/libs/Camera.js
@@ -0,0 +1,9 @@
+import { Matrix4 } from './Matrix4.js';
+import { Vector3 } from './Vector3.js';
+
+export default class Camera {
+  constructor() {
+    this.transform = Matrix4.identity();
+    this.position = Vector3.zero();
+  }
+}
