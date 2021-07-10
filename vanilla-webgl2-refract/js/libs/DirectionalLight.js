233a1a4da539bbb8111190d82cb4abf0227840e5
takumifukasawa <fukapon912@gmail.com>
2021-07-10 16:29:52 +0900

    refract

---
+++ vanilla-webgl2-refract/js/libs/DirectionalLight.js
@@ -0,0 +1,8 @@
+import { Vector3 } from './Vector3.js';
+
+export default class DirectionalLight {
+  constructor({ color, position }) {
+    this.color = color || Vector3.one();
+    this.position = position || Vector3.one();
+  }
+}
