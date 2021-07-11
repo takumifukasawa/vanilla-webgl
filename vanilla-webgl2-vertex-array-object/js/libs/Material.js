8d2c5f02e5f0ca47280a744de102146a3e4df196
takumifukasawa <fukapon912@gmail.com>
2021-07-11 19:52:45 +0900

    privateに移行

--- vanilla-webgl2-vertex-array-object/js/libs/Material.js
+++ vanilla-webgl2-vertex-array-object/js/libs/Material.js
@@ -2,6 +2,32 @@ import GPU from './GPU.js';
 import Shader from './Shader.js';
 
 export default class Material {
+  #uniforms;
+  #shader;
+  #primitiveType;
+  #blendType;
+  #transparent;
+
+  get uniforms() {
+    return this.#uniforms;
+  }
+
+  get shader() {
+    return this.#shader;
+  }
+
+  get primitiveType() {
+    return this.#primitiveType;
+  }
+
+  get blendType() {
+    return this.#blendType;
+  }
+
+  get transparent() {
+    return this.#transparent;
+  }
+
   constructor({
     gpu,
     vertexShader,
@@ -11,25 +37,25 @@ export default class Material {
     transparent,
     blendType,
   }) {
-    this.uniforms = uniforms;
-    this.shader = new Shader({
+    this.#uniforms = uniforms;
+    this.#shader = new Shader({
       gpu,
       vertexShader,
       fragmentShader,
     });
-    this.primitiveType = primitiveType || GPU.Primitives.Triangles;
-    this.transparent = !!transparent;
-    this.blendType = GPU.BlendTypes.None;
-    if (this.transparent && blendType) {
-      this.blendType = blendType;
+    this.#primitiveType = primitiveType || GPU.Primitives.Triangles;
+    this.#transparent = !!transparent;
+    this.#blendType = GPU.BlendTypes.None;
+    if (this.#transparent && blendType) {
+      this.#blendType = blendType;
     }
   }
 
   // getTextureUniforms() {
   //   const textureUniforms = [];
-  //   Object.keys(this.uniforms).forEach((name) => {
-  //     if (this.uniforms[name].type === GPU.UniformTypes.Texture2D) {
-  //       textureUniforms[name] = this.uniforms[name];
+  //   Object.keys(this.#uniforms).forEach((name) => {
+  //     if (this.#uniforms[name].type === GPU.UniformTypes.Texture2D) {
+  //       textureUniforms[name] = this.#uniforms[name];
   //     }
   //   });
   //   return textureUniforms;
@@ -44,48 +70,48 @@ export default class Material {
     cameraPosition,
   }) {
     const uniformModelMatrix =
-      this.uniforms[
-        Object.keys(this.uniforms).find((name) => name === 'uModelMatrix')
+      this.#uniforms[
+        Object.keys(this.#uniforms).find((name) => name === 'uModelMatrix')
       ];
     if (uniformModelMatrix) {
       uniformModelMatrix.data = modelMatrix;
     }
 
     const uniformInvModelMatrix =
-      this.uniforms[
-        Object.keys(this.uniforms).find((name) => name === 'uInvModelMatrix')
+      this.#uniforms[
+        Object.keys(this.#uniforms).find((name) => name === 'uInvModelMatrix')
       ];
     if (uniformInvModelMatrix) {
       uniformInvModelMatrix.data = modelMatrix.clone().inverse();
     }
 
     const uniformViewMatrix =
-      this.uniforms[
-        Object.keys(this.uniforms).find((name) => name === 'uViewMatrix')
+      this.#uniforms[
+        Object.keys(this.#uniforms).find((name) => name === 'uViewMatrix')
       ];
     if (uniformViewMatrix) {
       uniformViewMatrix.data = viewMatrix;
     }
 
     const uniformProjectionMatrix =
-      this.uniforms[
-        Object.keys(this.uniforms).find((name) => name === 'uProjectionMatrix')
+      this.#uniforms[
+        Object.keys(this.#uniforms).find((name) => name === 'uProjectionMatrix')
       ];
     if (uniformProjectionMatrix) {
       uniformProjectionMatrix.data = projectionMatrix;
     }
 
     const uniformNormalMatrix =
-      this.uniforms[
-        Object.keys(this.uniforms).find((name) => name === 'uNormalMatrix')
+      this.#uniforms[
+        Object.keys(this.#uniforms).find((name) => name === 'uNormalMatrix')
       ];
     if (uniformNormalMatrix) {
       uniformNormalMatrix.data = normalMatrix;
     }
 
     const uniformCameraPosition =
-      this.uniforms[
-        Object.keys(this.uniforms).find((name) => name === 'uCameraPosition')
+      this.#uniforms[
+        Object.keys(this.#uniforms).find((name) => name === 'uCameraPosition')
       ];
     if (uniformCameraPosition) {
       uniformCameraPosition.data = cameraPosition;
